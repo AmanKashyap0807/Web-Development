1c8382e48e04391fa03af855ebc2c258d6d74ae5
Aman Kashyap <akashyap_be23@thapar.edu>
2025-02-12 20:06:48 +0530

    math function

---
+++ JavaScript/Part 05/04.js
@@ -0,0 +1,21 @@
+// Math object
+
+console.log(Math.PI);
+console.log(Math.E);
+
+// for getting absolute value
+console.log(Math.abs(-12));
+
+console.log(Math.pow(2, 3));
+
+console.log(Math.floor(2.7));
+console.log(Math.ceil(2.3));
+
+// random function : provide any random value between 0 to 1 excuding 1
+console.log(Math.random());
+
+// generation of integer random number between 1 to 10 including 10
+console.log(Math.floor(Math.random() * 10) + 1);
+
+// generation of random number between 21 to 25
+console.log(Math.floor(Math.random() * 5) + 21);
