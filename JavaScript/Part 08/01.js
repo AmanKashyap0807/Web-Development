0277474433ba4cab642669039dc4be693ccc9cf8
Aman Kashyap <akashyap_be23@thapar.edu>
2025-03-16 12:06:14 +0530

    Add examples demonstrating the use of forEach() method with arrays

--- JavaScript/Part 08/01.js
+++ JavaScript/Part 08/01.js
@@ -1,1 +1,25 @@
+// Array Methods
+// forEach() : rakes a function as an argument and applies that function to each element of the array.
 
+let arr = [1, 2, 3, 4, 5];
+arr.forEach(ele => {
+    console.log(ele);
+});
+
+console.log("========================================");
+
+let student = [{
+    name: "John",
+    marks: 80
+}, {
+    name: "aman",
+    marks: 100
+},
+{
+    name: "aksra",
+    marks: 33
+}]
+
+student.forEach((ele, ind) => {
+    console.log(`${ind + 1} : ${ele.name} : ${ele.marks}`);
+})
