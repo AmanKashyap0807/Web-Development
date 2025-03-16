a7aad7c5db165e30440026ca1710607b90582939
Aman Kashyap <akashyap_be23@thapar.edu>
2025-03-16 10:49:53 +0530

    Add square function and interval printing example with corrections

--- JavaScript/Part 07/Assignment/Practice.js
+++ JavaScript/Part 07/Assignment/Practice.js
@@ -1,1 +1,17 @@
+// write a arrow function which return the squre of n
 
+let sqr = n => n * n;
+console.log(sqr(13));
+
+
+// write a function that prints Hello World! 5 times at intervals of 2 sec each
+
+const id = setInterval(() => {
+    console.log('Hello World!');
+}, 2000);
+// setTimeout(clearInterval(id), 10000);
+// this above line code is wrong as setTimeout() expects a function as the first argument, but we are passing the return value of clearInterval(id) which is undefined.
+
+setTimeout(() => {
+    clearInterval(id);
+}, 11000);
