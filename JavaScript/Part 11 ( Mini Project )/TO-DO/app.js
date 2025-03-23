d8c7e4fd968c0ade52f711029b2bc842160de208
AmanKashyap0807 <akashyap_be23@thapar.edu>
2025-03-23 13:04:20 +0530

    added task input page

--- JavaScript/Part 11 ( Mini Project )/TO-DO/app.js
+++ JavaScript/Part 11 ( Mini Project )/TO-DO/app.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const nameInput = document.querySelector('#name-input');
     const submitBtn = document.querySelector('#submit-name');
     let nameSpan = document.querySelector("span");
+    let content = document.querySelector('#content');
 
     nameInput.focus();
 
@@ -23,6 +24,37 @@ document.addEventListener('DOMContentLoaded', function () {
     nameInput.addEventListener('keydown', function (event) {
         if (event.key === "Enter") {
             showContent();
+        }
+    })
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+    let content = document.querySelector('#content');
+    let inputPage = document.querySelector('#taskInput');
+    let addBtn = document.querySelector(".addTaskBtn");
+    const addtrans = document.querySelector(".plusBtn");
+    const task = document.querySelector("#task");
+    let list = document.querySelector("#list ul");
+
+    addtrans.addEventListener('click', function () {
+        content.style.display = "none";
+        inputPage.style.display = "flex"
+        task.focus();
+    });
+
+    function taskAdd() {
+        let li = document.createElement("li");
+        li.textContent = task.value
+        list.appendChild(li);
+        task.value = "";
+        content.style.display = "block";
+        inputPage.style.display = "none";
+    }
+
+    addBtn.addEventListener('click', taskAdd);
+    task.addEventListener('keydown', function (event) {
+        if (event.key === "Enter") {
+            taskAdd();
         }
     })
 })
