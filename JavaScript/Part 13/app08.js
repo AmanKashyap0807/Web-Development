b5c8e1eb83d0935aafcf66cdf180f0f085b558c5
AmanKashyap0807 <akashyap_be23@thapar.edu>
2025-03-26 17:48:35 +0530

    added fetch with await

---
+++ JavaScript/Part 13/app08.js
@@ -0,0 +1,21 @@
+// the main Problems for which await is used 
+
+let url = "https://catfact.ninja/fact";
+
+async function getFact(url) {
+    let res = fetch(url);
+    console.log(res);
+    // if fetch takes time to fetch data from api then the execution of next line will be done and till then res is not defined
+    // so it might throw undefined 
+
+}
+
+getFact(url);
+
+// so we use await keyword to wait for the fetch to complete and then move to next line
+
+async function getFact2(url) {
+    let res = await fetch(url);
+    console.log(res);
+}
+getFact2(url);
