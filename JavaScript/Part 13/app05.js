355f3faba02b7022646e1a07a61744eedceee995
AmanKashyap0807 <akashyap_be23@thapar.edu>
2025-03-26 09:57:46 +0530

    API tester, Ajax, HTTP verbs, status codes

---
+++ JavaScript/Part 13/app05.js
@@ -0,0 +1,29 @@
+// WEBAPI testing : normaly by link for user
+// For developer two famous tools - postman & HOPPSCOTCH
+
+// postman neet to download and sign up so we will be using HOPPSCOTCH
+
+// HOPPSCOTCH : https://hoppscotch.io/
+// get option : check the status, latency and size of the data with provide data in different format
+
+
+// what is Ajax?
+// Ajax : Asynchronous JavaScript and XML
+// it take some time to get data from API so to continue the work its important to make it dynamic which is basically AJax call
+
+// in past the api request come back in the form of XML thats why its name is Ajax but technally it should be AJAJ (Asynchronous JavaScript and JSON)
+
+// Http verbs
+// GET : get the data
+// POST : add the data
+// PUT : update the data
+// DELETE : delete the data
+// we can see example of it using HOPPSCOTCH
+
+// API request Status codes
+// 200 : data get succefully
+// 404 : API doesn't exist
+// 400 : bad request
+// 500 : internal server error
+// there are different many status codes -> refer to MDN
+
