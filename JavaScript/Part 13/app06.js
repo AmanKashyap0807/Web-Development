8fd9cbad43c573304509cb3ebea44cd6b9fd55a5
AmanKashyap0807 <akashyap_be23@thapar.edu>
2025-03-26 17:10:24 +0530

    explained URL detail, its manipulation and HTTPS header

---
+++ JavaScript/Part 13/app06.js
@@ -0,0 +1,26 @@
+// Adding information in url
+// ----------------------------------------------------------------------------
+// query string method
+// ----------------------------------------------------------------------------
+// we can change the key value pair on any url to get desired result
+
+
+
+// we can change the value of these variable to get the desired result
+
+// ----------------------------------------------------------------------------
+// API route method
+// ----------------------------------------------------------------------------
+// in any url there are variable part mostly denoted by
+// /:id
+// :num
+// :q
+// <id>
+// <num>
+// we can use this variable to change their value and get the data
+
+// invalid key value pair are ignored by the browser
+
+// HTTPS header: there are different info which automatically goes from client side by browser to server side
+// above are just key value pair
+
