3bc1ab91492456ddf1a99db8331e7719e907573f
AmanKashyap0807 <akashyap_be23@thapar.edu>
2026-04-09 18:22:17 +0530

    code clarity and basic

---
+++ JavaScript/freeCodeCamp/Varibles and Strings/Understanding code clarity/02.js
@@ -0,0 +1,18 @@
+// Comments
+
+// Two ways for comment
+// single line comment : "//"
+// multi line comment : "/* */"
+
+// BENEFIT
+/* Other developers working on the project will understand the purpose of this code.
+It helps prevent unnecessary changes or deletions without consulting the team, which could lead to bugs or issues.*/
+
+// Not to do
+/*While comments are useful in programming, it is important to avoid over-commenting. You don't need to comment on every single line of code, especially if the code is straightforward and self-explanatory.*/
+// The goal is to enhance readability, not clutter the code with unnecessary explanations.
+// It is also important not to use comments to help explain away confusing, overly complicated, or poorly written code. In those situations, it is best to refactor, or change, your code so other developers will better understand what is going on.
+
+// Interview question
+// Q1. Tell 5 benefit of using comments in code
+
