f40e8e84587b6ed5fbd8905391afaea8579a1377
AmanKashyap0807 <akashyap_be23@thapar.edu>
2026-04-09 22:08:47 +0530

    user interation with prompt() method

---
+++ JavaScript/freeCodeCamp/Varibles and Strings/Working with strings in JS/05.js
@@ -0,0 +1,13 @@
+// What Is the prompt() Method, and How Does It Work?
+
+const userName = prompt("Enter your name : ", "noone");
+
+// DO not run it rn
+// prompt(x,y)
+// x-> message asking the info from the user
+// y-> default value set on vairable if user does not answer
+
+// It will halt the execution of the script until the user interatcs with the dialog box.
+
+/*One other point to consider is that while prompt() is useful for quick testing or small applications, it's generally avoided in modern, complex web applications due to its disruptive nature and inconsistent behavior across different browsers.*/
+
