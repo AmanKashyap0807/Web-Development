85e82182982f11a20221950b6dcd5ae1b9b97be1
AmanKashyap0807 <akashyap_be23@thapar.edu>
2026-04-09 18:35:58 +0530

    dynamic and static typing

---
+++ JavaScript/freeCodeCamp/Varibles and Strings/Working with Datatypes/01.js
@@ -0,0 +1,16 @@
+// What Is Dynamic Typing in JavaScript, and How Does It Differ from Statically Typed Languages?
+
+// Js is dynamically typed language : no need to specify the data type of a variable, Data type automatic assgined based on the assigned value on it
+// allows flexibility to change type of a variable throughout the code
+let Name = "Aman";
+Name = 143;
+
+// C and C++ is statically typed language, Java (not js) is also a statically typed language
+
+
+// Interview question :
+// Q1. If we have dynamically types language then why need of static type 
+
+
+
+// A1. while dynamically typed language is flexible but it introduces some error so to make sure, for more realiability and code clarity we use static type language like C and C++
